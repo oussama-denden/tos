589630daa691e6032c0839f03020356892b9c7c9
anisselmane <anis.selmane@tritux.com>
2014-10-27 15:33:52 +0100

    fix unit test

--- src/test/java/com/nordnet/opale/draft/service/test/AjouterReferenceExterneTest.java
+++ src/test/java/com/nordnet/opale/draft/service/test/AjouterReferenceExterneTest.java
@@ -3,14 +3,19 @@ package com.nordnet.opale.draft.service.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.spring.annotation.SpringBean;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.nordnet.opale.business.ReferenceExterneInfo;
 import com.nordnet.opale.domain.draft.Draft;
 import com.nordnet.opale.draft.test.GlobalTestCase;
+import com.nordnet.opale.draft.test.generator.DraftInfoGenerator;
 import com.nordnet.opale.exception.OpaleException;
 import com.nordnet.opale.service.draft.DraftService;
 import com.nordnet.opale.test.utils.OpaleMultiSchemaXmlDataSetFactory;
@@ -34,18 +39,31 @@ public class AjouterReferenceExterneTest extends GlobalTestCase {
 	private DraftService draftService;
 
 	/**
+	 * {@link DraftInfoGenerator}.
+	 */
+	@SpringBean("draftInfoGenerator")
+	private DraftInfoGenerator draftInfoGenerator;
+
+	/**
 	 * Test ajouter reference externe valid.
 	 * 
 	 * @throws OpaleException
+	 *             the opale exception
+	 * @throws JsonParseException
+	 *             the json parse exception
+	 * @throws JsonMappingException
+	 *             the json mapping exception
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
 	 *             {@link OpaleException}
 	 */
 	@Test
 	@DataSet(factory = OpaleMultiSchemaXmlDataSetFactory.class, value = { "/dataset/ajouter-reference-externe.xml" })
-	public void TestAjouterReferenceExterneValid() throws OpaleException {
+	public void TestAjouterReferenceExterneValid() throws OpaleException, JsonParseException, JsonMappingException,
+			IOException {
 		LOGGER.info("Debut methode :::TestAjouterReferenceExterneValid");
-		ReferenceExterneInfo referenceExterneInfo = new ReferenceExterneInfo();
-		referenceExterneInfo.setUser("userTest");
-		referenceExterneInfo.setReferenceExterne("REF-DRAFT-EXTERNE-1");
+		ReferenceExterneInfo referenceExterneInfo = draftInfoGenerator.getObjectFromJsonFile(
+				ReferenceExterneInfo.class, "./requests/ajouterReferenceExterne.json");
 		draftService.ajouterReferenceExterne("REF-DRAFT-1", referenceExterneInfo);
 		Draft draft = draftService.getDraftByReference("REF-DRAFT-1");
 		assertNotNull(draft.getReferenceExterne());
