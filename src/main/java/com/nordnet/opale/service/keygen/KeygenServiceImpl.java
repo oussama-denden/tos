7406ba27f8f517c29ee60b6869dfa6ba50f49841
anisselmane <anis.selmane@tritux.com>
2014-11-19 09:03:00 +0100

    keygen in progress

--- src/main/java/com/nordnet/opale/service/keygen/KeygenServiceImpl.java
+++ src/main/java/com/nordnet/opale/service/keygen/KeygenServiceImpl.java
@@ -4,10 +4,12 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nordnet.opale.domain.Keygen;
 import com.nordnet.opale.domain.commande.Commande;
 import com.nordnet.opale.domain.draft.Draft;
 import com.nordnet.opale.enums.Prefix;
 import com.nordnet.opale.repository.KeygenRepository;
+import com.nordnet.opale.util.Constants;
 
 /**
  * L'implementation de service {@link KeygenService}.
@@ -42,7 +44,24 @@ public class KeygenServiceImpl implements KeygenService {
 				clazz.equals(Draft.class) ? Prefix.Dra.toString() : clazz.equals(Commande.class) ? Prefix.Cmd
 						.toString() : null;
 
-		String reference = keygenRepository.getReference(clazz.getName(), prefix != null ? prefix.toString() : null);
+		Keygen keygen = keygenRepository.findDernier(clazz.getName());
+
+		Keygen keygen2 = new Keygen();
+		int inc = 0;
+		if (keygen != null) {
+			inc = Integer.parseInt(keygen.getReferenceDraft()) + 1;
+		} else {
+			inc = Integer.parseInt(Constants.REF_DRAFT_INIT) + 1;
+		}
+
+		// generer la nouvelle reference draft.
+
+		String newReferenceDraft = String.format("%08d", inc);
+		keygen2.setReferenceDraft(newReferenceDraft);
+		keygen2.setEntite(clazz.getName());
+		keygenRepository.save(keygen2);
+
+		String reference = keygen != null ? keygen.getReferenceDraft() : Constants.REF_DRAFT_INIT;
 		referenceRetour =
 				clazz.equals(Draft.class) ? Prefix.Dra + "-" + reference : clazz.equals(Commande.class) ? Prefix.Cmd
 						+ "-" + reference : reference;
